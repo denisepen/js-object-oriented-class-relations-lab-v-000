96792dd5ecb258524bcc78dbf7d9f1577dd9050b
Denise Pendleton <dpendl1@hotmail.com>
2018-05-31 15:13:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,13 +27,13 @@ class Trip{
   // }
   driver(){
     return store.drivers.find(driver => {
-      return driver.id = this.driverId
+      return driver.id === this.driverId
     })
   }
 
 passenger(){
   return store.passengers.find(passenger => {
-    return passenger.id = this.passengerId
+    return passenger.id === this.passengerId
   })
 }
   // passenger(){
