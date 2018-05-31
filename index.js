1d09d787e7ad3839dcd61d6c42e03a0a4eea828b
Denise Pendleton <dpendl1@hotmail.com>
2018-05-31 15:03:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ class Trip{
   constructor(driver, passenger){
     this.id = tripId++
     this.passenger = passenger
-    this.driverId = driver.id
-     this.passengerId = passenger.id
+    // this.driverId = driver.id
+    //  this.passengerId = passenger.id
     if(driver){
      this.driverId = driver.id
       driver.tripId = this.id
     }
-    
+
     if(passenger){
       this.passengerId = passenger.id
       passenger.tripId = this.id
     }
-    
+
     store.trips.push(this)
   }
 
