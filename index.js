43bdc00b1532742860955d942b96477560be0734
Denise Pendleton <dpendl1@hotmail.com>
2018-05-31 15:16:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ let tripId = 0
 class Trip{
   constructor(driver, passenger){
     this.id = tripId++
-    // this.passenger = passenger
-    // this.driver = driver
+  
     // this.driverId = driver.id
     //  this.passengerId = passenger.id
     if(driver){
