ba897e48ceead271c3036adc011e63e0bf099775
Denise Pendleton <dpendl1@hotmail.com>
2018-05-31 13:10:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,69 @@
+let store = {drivers: [], trips: [], passengers: []}
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
+class Trip{
+  constructor(driver, passenger){
+    this.id = ++tripId
+  //  this.passenger = passenger
+    this.passengerId = passenger.id
+    // this.driver = driver
+    this.driverId = driver.id
+    this.tripId = this.id
+
+
+    // this.passenger = function() {
+    //    return store.passengers.find(function(passenger){
+    //      return  passenger.tripId === this.id
+    //    })
+    //  }
+
+     this.driver = function() {
+       return store.drivers.find(function(driver){
+         return this.id === driver.trip.id
+       })
+     }
+
+    store.trips.push(this)
+  }
+
+
+}
+
+
+ class Driver{
+
+  constructor(name){
+
+    this.id = ++driverId
+    this.name = name
+    store.drivers.push(this)
+        }
+        // if(trip){
+        //   this.tripId = trip.id}
+
+      trips(){
+        return store.trips.filter(function(trip){
+          return trip.driver.id === this.id
+        })
+      }
+}
+
+
+class Passenger{
+  constructor(name){
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  }
+  // this.trips = function(){
+  //   return store.trips.filter(function(trip){
+  //     return this.id === trip.passengerId
+  //   })
+  // }
+}
+
+
+
+//
