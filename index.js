a21b37600f4b5d76d4961598659e699fa529abbd
Denise Pendleton <dpendl1@hotmail.com>
2018-05-31 15:40:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,79 @@
+let store = {drivers: [], trips: [], passengers: []}
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
+class Trip{
+  constructor(driver, passenger){
+    this.id = ++tripId
+
+    if(driver){
+     this.driverId = driver.id
+      driver.tripId = this.id
+    }
+
+    if(passenger){
+      this.passengerId = passenger.id
+      passenger.tripId = this.id
+    }
+
+    store.trips.push(this)
+  }
+
+  driver(){
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId
+    })
+  }
+
+passenger(){
+  return store.passengers.find(passenger => {
+    return passenger.id === this.passengerId
+    })
+  }
+}
+
+
+ class Driver{
+
+  constructor(name){
+
+    this.id = ++driverId
+    this.name = name
+    store.drivers.push(this)
+        }
+
+        trips(){
+          return store.trips.filter(trip => {
+            return trip.driverId === this.id
+          })
+        }
+
+      passengers(){
+        return this.trips().map(trip => {
+          return trip.passenger()
+        })
+        }
+}
+
+
+class Passenger{
+  constructor(name){
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  }
+
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id
+    })
+  }
+
+  drivers(){
+    return this.trips().map(trip => {
+      return trip.driver()
+    })
+    }
+
+}
